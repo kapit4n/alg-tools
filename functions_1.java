5bb03f916be7af10a4b58151d4b7c0e55ed5610f
lartl <luis.arce22@gmail.com>
2018-12-12 22:56:06 -0400

    Adding java functions

---
+++ functions_1.java
@@ -0,0 +1,27 @@
+// COMMON FUNCTIONS USED IN AN ALGORITHM WITH JAVA
+// FLAT. Default depth is 1
+String[][] = new String[][]{{"a", "b"}, {"c", "c"}};
+Stream<String[]> temp = Arrays.stream(data);
+Stream<String> stringStream = temp.flatMap(x -> Arrays.stream(x));
+
+// GROUP BY
+
+// SORTING
+
+// FILTER
+
+// FIND, gets the first element
+
+// SPLIT
+
+// SUBSTRING
+
+// FOREACH
+
+// WHILE
+
+// ASSIGN
+
+// MAP
+
+// REDUCE
--- functions_1.js
+++ functions_1.js
@@ -9,38 +9,45 @@ var groupBy = function(xs, key) {
     (rv[x[key]] = rev[x[key]] || []).push(x);
     return rv;
   }, {});
-}[
-  // SORTING
-  ("banana", "orange", "apple", "mango")
-].sort();
+};
+
+// SORTING
+["banana", "orange", "apple", "mango"].sort();
 ["banana", "orange", "apple", "mango"].reverse();
 [1, 4, 2, 7, 0].sort((a, b) => a - b);
 [1, 4, 2, 7, 0].sort((a, b) => b - a);
 
-// FILTER
+// FILTER, findAll equivalent
 [1, 2, 4, 6].filter(num => num > 2);
 
-// find, gets the first element
+// FIND, gets the first element
 var found = [1, 2, 5].find(function(element) {
   return element > 2;
 });
-// split
+
+// SPLIT
 var res = str.split(" ", 2); //  get the first two words
+
 // SUBSTRING
 var res = str.substring(1, 4);
+
 // FOREACH
 [1, 2, 3].forEach(function(element) {
   console.log(element);
 });
+
 // WHILE
 while (i < 2) {
   accum += i;
   i++;
 }
+
 // ASSIGN
 const object2 = Object.assign({ c: 3, d: 4 }, object1);
+
 // MAP
 [1, 2].map(x => x * 2);
+
 // REDUCE
 const reducer = (acum, current) => accum + currentValue;
 [1, 2, 3].reduce(reducer);
